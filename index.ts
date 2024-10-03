321ca7bb7d95d83a1a07f2bcbb51083a352daa3f
Lethal15004 <Phamvahuy2004@gmail.com>
2024-10-03 19:30:10 +0700

    Thêm pug và giao diện đơn giản tours

--- index.ts
+++ index.ts
@@ -1,17 +1,21 @@
 import express,{Express, Request, Response,NextFunction} from 'express';
 import dotenv from 'dotenv';//Nhúng dotenv từ module dotenv
 import bodyParser from'body-parser';//Nhúng body-parser từ module body-parser
+import { title } from 'process';
 dotenv.config();//Thêm config cho dotenv
 
 const app: Express = express();
 const port : number | string =process.env.PORT ||3000;
 
+//Phần view engine -> Để render file pug (Quan trọng phải có)
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'pug');
 app.use(express.static(`${__dirname}/public`));//Định tuyến file tĩnh (Quan trọng phải có)
 
-
-
-app.get('/', (req:Request, res:Response) => {
-    res.send("Trang chủ");
+app.get('/tours', (req:Request, res:Response) => {
+    res.render('client/pages/tours/index',{
+        title:"Danh sách tour"
+    })
 })
 
 app.listen(port,()=>{
