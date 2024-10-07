b8b4df4a1251f6c9b2d7367098d0115ddcade434
Lethal15004 <Phamvahuy2004@gmail.com>
2024-10-07 13:01:32 +0700

    Làm trang Danh sách tour theo Danh mục (bên client)

--- controller/tour.controller.ts
+++ controller/tour.controller.ts
@@ -1,14 +1,45 @@
 import { Request,Response } from "express";
 import Tour from '../model/tour.model';//Nhúng model Tour vào dự án
+import sequelize from "../config/database";
+import { QueryTypes } from "sequelize";
 
 export const index = async(req: Request, res: Response) => {
-    const tours= await Tour.findAll({
-        where:{
-            deleted:false,
-            status:'active'
-        },
-        raw:true // Để đảm bảo dữ liệu trả về là dạng mảng và đẹp hơn
+    const slugCategory=`${req.params.slugCategory}`;
+    /*
+        SELECT tours.*, price * (1 - discount/100) AS price_special
+        FROM tours
+        JOIN tours_categories ON tours.id = tours_categories.tour_id
+        JOIN categories ON tours_categories.category_id = categories.id
+        WHERE
+        categories.slug = 'du-lich-trong-nuoc'
+        AND categories.deleted = false
+        AND categories.status = 'active'
+        AND tours.deleted = false
+        AND tours.status = 'active';
+    */
+    const tours= await sequelize.query(`
+        SELECT tours.*, ROUND(price*(1-discount/100)) as price_special
+        FROM tours
+        JOIN tours_categories ON tours.id=tours_categories.tour_id
+        JOIN categories ON tours_categories.category_id=categories.id
+        WHERE
+            categories.slug='${slugCategory}'
+            AND categories.deleted=false
+            AND categories.status='active'
+            AND tours.deleted=false
+            AND tours.status='active'
+        `,{
+        type:QueryTypes.SELECT,
     });
+    for(const item of tours){
+        if(item['images']){
+            const arrayImages = JSON.parse(item['images']);
+            if(arrayImages.length>0){
+                item['image']=arrayImages[0];
+            }
+        }
+        item['price_special']=parseInt(item['price_special']);
+    }
     res.render('client/pages/tours/index',{
         title:"Danh sách tour",
         tours:tours
--- routes/tour.route.ts
+++ routes/tour.route.ts
@@ -3,6 +3,5 @@ const router : Router=express.Router();
 
 import * as tourController from '../controller/tour.controller';
 
-router.get('/',tourController.index);
-
+router.get('/:slugCategory',tourController.index);
 export default router;
